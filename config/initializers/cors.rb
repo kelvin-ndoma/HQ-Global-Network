27b8010166872680d5f6887aef18c272cd308f3e
kelvin-ndoma <kelvinndomamutua@gmail.com>
2024-10-17 23:11:13 +0300

    cors.rb setup for local development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,18 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5173'
-      resource '*', 
-        headers: :any, 
-        methods: [:get, :post, :put, :patch, :delete, :options, :head, :options],
-        credentials: true
-    end
-    allow do
-        origins 'http://localhost:5173'
-        resource '*', 
-          headers: :any, 
-          methods: [:get, :post, :put, :patch, :delete, :options, :head, :options],
-          credentials: true
-      end
-     
+  allow do
+    origins 'http://localhost:5173'  # Allow the local frontend
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true  # Enable credentials (cookies, session data)
   end
-  
+
+  # Uncomment this block when you deploy your frontend
+  # allow do
+  #   origins 'https://your-frontend-domain.com'  # Update this to your actual frontend URL once deployed
+  #   resource '*', 
+  #     headers: :any, 
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
+end
